c4621f4f8015ed5e108fe096c43b24ce00436acf
Martin Hradil <mhradil@redhat.com>
2024-09-16 18:17:08 +0000

    make jest work

--- config/setupTests.js
+++
@@ -1,7 +0,0 @@
-import '@testing-library/jest-dom/extend-expect';
-import 'jest-canvas-mock';
-import React from 'react';
-
-React.useLayoutEffect = React.useEffect;
-
-global.React = React;
--- jest.config.js
+++ jest.config.js
@@ -1,19 +1,15 @@
 module.exports = {
-  testEnvironment: 'jsdom',
-  collectCoverage: true,
-  verbose: true,
-  collectCoverageFrom: ['src/**/*.js', '!src/**/stories/*'],
-  coverageDirectory: './coverage/',
+  moduleNameMapper: {
+    '\\.(css|scss)$': 'identity-obj-proxy',
+  },
   roots: ['<rootDir>/src/'],
-  setupFilesAfterEnv: ['<rootDir>/config/setupTests.js'],
+  testEnvironment: 'jsdom',
+  testEnvironmentOptions: {
+    url: 'http://localhost:4000/ansible/ansible-dashboard',
+  },
   transformIgnorePatterns: [
     '/node_modules/(?!@redhat-cloud-services)',
     '/node_modules/(?!@patternfly)',
   ],
-  moduleNameMapper: {
-    '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':
-      '<rootDir>/src/test/__mocks__/fileMock.js',
-    '\\.(css|scss)$': 'identity-obj-proxy',
-  },
-  testURL: 'http://localhost:4000/beta/ansible/ansible-dashboard',
+  verbose: true,
 };
